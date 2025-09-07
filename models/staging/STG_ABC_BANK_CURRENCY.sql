e4e6a2ded86fe6951ffa1ec6c294df5b1dfd7307
okv627@gmail.com <okv627@gmail.com>
2025-09-07 20:11:12 +0000

    Define and configure staging models for country, currency, and exchange

---
+++ models/staging/STG_ABC_BANK_CURRENCY.sql
@@ -0,0 +1,48 @@
+{{ config(materialized='ephemeral') }}
+
+WITH
+
+src_data AS (
+    SELECT
+        AlphabeticCode      AS CURRENCY_CODE,     -- text
+        NumericCode         AS CURRENCY_NUM_CODE, -- text or number
+        DecimalDigits       AS DECIMAL_DIGITS,    -- number
+        CurrencyName        AS CURRENCY_NAME,     -- text
+        Locations           AS LOCATIONS,         -- text
+        LOAD_TS             AS LOAD_TS_UTC,       -- TIMESTAMP_NTZ
+        'SEED.currency_ISO_4217' AS RECORD_SOURCE
+
+    FROM {{ source('seeds', 'currency_ISO_4217') }}
+),
+
+default_record AS (
+    SELECT
+        '-1'                           AS CURRENCY_CODE,
+        NULL                           AS CURRENCY_NUM_CODE,
+        2                              AS DECIMAL_DIGITS,
+        'Missing'                      AS CURRENCY_NAME,
+        NULL                           AS LOCATIONS,
+        TO_TIMESTAMP_NTZ('2020-01-01') AS LOAD_TS_UTC,
+        'Missing'                      AS RECORD_SOURCE
+),
+
+with_default_record AS (
+    SELECT * FROM src_data
+    UNION ALL
+    SELECT * FROM default_record
+),
+
+hashed AS (
+    SELECT
+        -- natural key for this dim is alphabetic code
+        CURRENCY_CODE AS CURRENCY_HKEY,
+
+        -- change fingerprint across descriptive attributes
+        CONCAT_WS('|', CURRENCY_CODE, CURRENCY_NAME,
+                       DECIMAL_DIGITS, LOCATIONS
+        ) AS CURRENCY_HDIFF,
+        *
+    FROM with_default_record
+)
+
+SELECT * FROM hashed;
---
+++ models/staging/STG_ABC_BANK_EXCHANGE.sql
@@ -0,0 +1,62 @@
+{{ config(materialized='ephemeral') }}
+
+WITH
+
+src_data AS (
+    SELECT
+        ID              AS EXCHANGE_CODE,
+        Name            AS EXCHANGE_NAME,
+        Country         AS COUNTRY_NAME,
+        City            AS CITY_NAME,
+        Zone            AS TIMEZONE_NAME,
+        Delta           AS TZ_DELTA_HOURS,
+        DST_period      AS DST_PERIOD_DESC,
+        Open            AS OPEN_LOCAL,
+        Close           AS CLOSE_LOCAL,
+        Lunch           AS LUNCH_LOCAL,
+        Open_UTC        AS OPEN_UTC,
+        Close_UTC       AS CLOSE_UTC,
+        Lunch_UTC       AS LUNCH_UTC,
+        LOAD_TS         AS LOAD_TS_UTC,
+        'SEED.exchange' AS RECORD_SOURCE
+
+    FROM {{ source('seeds', 'exchange') }}
+),
+
+default_record AS (
+    SELECT
+        '-1' AS
+        EXCHANGE_CODE,
+        'Missing' AS EXCHANGE_NAME,
+        NULL AS COUNTRY_NAME,
+        NULL AS CITY_NAME,
+        NULL AS TIMEZONE_NAME,
+        NULL AS TZ_DELTA_HOURS,
+        NULL AS DST_PERIOD_DESC,
+        NULL AS OPEN_LOCAL,
+        NULL AS CLOSE_LOCAL,
+        NULL AS LUNCH_LOCAL,
+        NULL AS OPEN_UTC,
+        NULL AS CLOSE_UTC,
+        NULL AS LUNCH_UTC,
+        TO_TIMESTAMP_NTZ('2020-01-01') AS LOAD_TS_UTC,
+        'Missing' AS RECORD_SOURCE
+),
+
+with_default_record AS (
+    SELECT * FROM src_data
+    UNION ALL
+    SELECT * FROM default_record
+),
+
+hashed AS (
+    SELECT
+        EXCHANGE_CODE AS EXCHANGE_HKEY,
+        CONCAT_WS('|', EXCHANGE_CODE, EXCHANGE_NAME, COUNTRY_NAME,
+                       CITY_NAME, TIMEZONE_NAME, OPEN_UTC, CLOSE_UTC
+        ) AS EXCHANGE_HDIFF,
+        *
+    FROM with_default_record
+)
+
+SELECT * FROM hashed;
