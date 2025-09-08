1f733e9084bf37084e2d84632e27e535a2c663b9
okv627@gmail.com <okv627@gmail.com>
2025-09-08 18:30:25 +0000

    Add macro to normalize report date for position fact table

---
+++ macros/normalize_report_date.sql
@@ -0,0 +1,15 @@
+{% macro normalize_report_date(col) %}
+(
+    try_to_date(
+        case
+            -- Fix dates like 0021-04-09 by replacing leading "00" with "20"
+            when substr(trim({{ col }}::varchar), 1, 2) = '00'
+            then '20' || substr(trim({{ col }}::varchar), 3)
+            
+            -- Otherwise, keep the value as-is
+            else trim({{ col }}::varchar)
+        end,
+        'YYYY-MM-DD'
+    )
+)
+{% endmacro %}
