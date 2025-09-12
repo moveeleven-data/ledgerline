449cf8fca5b44ce9b7dba43e1f538c89df401bbd
Matthew Tripodi <okv627@gmail.com>
2025-09-12 00:16:00 +0000

    simplify logic in no hash collision test

--- macros/tests/hash_collision_free.sql
+++ macros/tests/hash_collision_free.sql
@@ -1,19 +1,11 @@
 {% test hash_collision_free(model, column_name, hashed_fields) %}
-{% if hashed_fields is string %}
-  {% set fields = [hashed_fields] %}
-{% elif hashed_fields is sequence %}
-  {% set fields = hashed_fields %}
-{% else %}
-  {% set fields = [] %}
-{% endif %}
-
-{% set parts = [ column_name ~ " as hash" ] + (fields | map('trim') | list) %}
-{% set select_list = parts | join(', ') %}
 
 with
 
 all_tuples as (
-  select distinct {{ select_list }}
+  select distinct
+    {{ column_name }} as hash,
+    {{ hashed_fields | join(', ') }}
   from {{ model }}
 ),
 
@@ -21,7 +13,7 @@ validation_errors as (
   select hash, count(*) as cnt
   from all_tuples
   group by hash
-  having count(*) > 1
+  having cnt > 1
 )
 
 select * from validation_errors
