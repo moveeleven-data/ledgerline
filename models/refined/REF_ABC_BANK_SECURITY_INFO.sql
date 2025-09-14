65e4e7e32d1939819ce807ccde9015677f01921f
Matthew Tripodi <okv627@gmail.com>
2025-09-14 15:40:33 +0000

    Finalize HIST model for Securities dimension

--- models/refined/REF_ABC_BANK_SECURITY_INFO.sql
+++ models/refined/REF_ABC_BANK_SECURITY_INFO.sql
@@ -1,8 +1,9 @@
 with
 current_from_snapshot as (
-    {{ current_from_snapshot(
-          snsh_ref = ref('SNSH_ABC_BANK_SECURITY_INFO')
-       ) }}
+    {{ current_from_history(
+          history_rel = ref('HIST_ABC_BANK_SECURITY_INFO'),
+          key_column = 'SECURITY_HKEY',
+    ) }}
 )
 select *
 from current_from_snapshot
