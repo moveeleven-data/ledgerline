f56f57450274e93262d920765b26a5b04410497f
Matthew Tripodi <okv627@gmail.com>
2025-09-14 23:43:05 +0000

    Implement deletion-aware change tracking in HIST position table

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -2,7 +2,7 @@ with
 
 position as (
     {{ current_from_history(
-        history_rel = ref('HIST_ABC_BANK_POSITION'), 
+        history_rel = ref('HIST_ABC_BANK_POSITION_WITH_CLOSING'), 
         key_column = 'POSITION_HKEY',
     ) }}
 )
@@ -14,7 +14,7 @@ select
     p.* exclude (SECURITY_CODE)
     , coalesce(s.SECURITY_CODE, '-1') as SECURITY_CODE
     , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
-    , ROUND(UNREALIZED_PROFIT / COST_BASE, 5)*100 as UNREALIZED_PROFIT_PCT
+    , ROUND(DIV0(UNREALIZED_PROFIT, COST_BASE), 5)*100 as UNREALIZED_PROFIT_PCT
 from position as p
 left outer join security as s
     on(s.SECURITY_CODE = p.SECURITY_CODE)
