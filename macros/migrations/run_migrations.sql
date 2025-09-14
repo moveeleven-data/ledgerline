b94bf70c76003b22c9ee3a412a2f5b9c94c4d7f3
Matthew Tripodi <okv627@gmail.com>
2025-09-14 22:46:50 +0000

    add migration macro for removing unused schemas

---
+++ macros/migrations/run_migrations.sql
@@ -0,0 +1,19 @@
+/** USAGE:
+ # -- Adding a migration macro:
+ # {#% do run_migration('V003_drop_table', database, schema_prefix) %#}
+ #
+ # -- How to report there are no migrations to run
+ # {#% do log("No migrations to run.", info=True) %#}
+ #
+ # !! Remove the # from the above lines to uncomment
+ # !! Update the string with the macro name !!
+ */
+ 
+{% macro run_migrations(
+        database = target.database,
+        schema_prefix = target.schema
+) -%}
+
+{% do run_migration('V003_drop_table', database, schema_prefix) %}
+
+{%- endmacro %}
