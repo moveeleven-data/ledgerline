df32919e7242479b8f7a8bbadec3cbe58ef79633
Matthew Tripodi <okv627@gmail.com>
2025-09-17 18:43:02 +0000

    update docs

--- macros/field_lists.sql
+++ macros/field_lists.sql
@@ -1,13 +1,13 @@
 {% macro abc_bank_position_diff_fields() %}
   [
-    'account_code',
-    'security_code',
-    'security_name',
-    'exchange_code',
-    "to_varchar(report_date, 'YYYY-MM-DD')",
-    'quantity',
-    'cost_base',
-    'position_value',
-    'currency_code'
+    'account_code'
+    , 'security_code'
+    , 'security_name'
+    , 'exchange_code'
+    , "to_varchar(report_date, 'YYYY-MM-DD')"
+    , 'quantity'
+    , 'cost_base'
+    , 'position_value'
+    , 'currency_code'
   ]
 {% endmacro %}
