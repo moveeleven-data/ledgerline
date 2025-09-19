c12c36aff1f350c91f5b9cc3bb9e652a8557e467
Matthew Tripodi <okv627@gmail.com>
2025-09-19 14:35:24 +0000

    add singular test for fact position grain

--- models/marts/portfolio/DIM_EXCHANGE.sql
+++ models/marts/portfolio/DIM_EXCHANGE.sql
@@ -1,17 +1,17 @@
 select
-    exchange_hkey   as exchange_key
-  , exchange_code   as exchange_code
-  , exchange_name   as exchange_name
-  , country_name    as country_name
-  , city_name       as city_name
-  , timezone_name   as timezone_name
-  , tz_delta_hours  as tz_delta_hours
+    exchange_hkey                         as exchange_key
+  , exchange_code                         as exchange_code
+  , exchange_name                         as exchange_name
+  , country_name                          as country_name
+  , city_name                             as city_name
+  , timezone_name                         as timezone_name
+  , cast(tz_delta_hours as number(38,0))  as tz_delta_hours
   , dst_period_desc as dst_period_desc
-  , open_local      as open_local
-  , close_local     as close_local
-  , lunch_local     as lunch_local
-  , open_utc        as open_utc
-  , close_utc       as close_utc
-  , lunch_utc       as lunch_utc
+  , open_local                            as open_local
+  , close_local                           as close_local
+  , lunch_local                           as lunch_local
+  , open_utc                              as open_utc
+  , close_utc                             as close_utc
+  , lunch_utc                             as lunch_utc
 from {{ ref('REF_EXCHANGE_ABC_BANK') }}
 where exchange_code <> '-1'
--- models/marts/portfolio/FACT_POSITION.sql
+++ models/marts/portfolio/FACT_POSITION.sql
@@ -2,12 +2,19 @@ with
 
 position_normalized as (
     select
-          report_date
-        , quantity
-        , cost_base
-        , position_value
-        , unrealized_profit
-        , unrealized_profit_pct
+          report_date::date                                   as report_date
+        , cast(quantity as number(38,0))                      as quantity
+        , cast(cost_base as number(38,2))                     as cost_base
+        , cast(position_value as number(38,2))                as position_value
+        , cast(position_value - cost_base as number(38,2))    as unrealized_profit
+        , cast(
+             round(
+                 (position_value - cost_base)
+                 / nullif(cost_base, 0)
+                 * 100
+                 , 5
+             ) as number(38,5)
+        ) as unrealized_profit_pct
         , coalesce(account_code,  '-1') as account_code_nk
         , coalesce(security_code, '-1') as security_code_nk
         , coalesce(exchange_code, '-1') as exchange_code_nk
@@ -17,16 +24,16 @@ position_normalized as (
 
 , position_enriched as (
   select
-        dim_account.account_key
-      , dim_security.security_key   as security_key
-      , dim_exchange.exchange_key
-      , dim_currency.currency_key
-      , pos_norm.report_date::date  as report_date
-      , pos_norm.quantity
-      , pos_norm.cost_base
-      , pos_norm.position_value
-      , pos_norm.unrealized_profit
-      , pos_norm.unrealized_profit_pct
+        dim_account.account_key         as account_key
+      , dim_security.security_key       as security_key
+      , dim_exchange.exchange_key       as exchange_key
+      , dim_currency.currency_key       as currency_key
+      , pos_norm.report_date            as report_date
+      , pos_norm.quantity               as quantity
+      , pos_norm.cost_base              as cost_base
+      , pos_norm.position_value         as position_value
+      , pos_norm.unrealized_profit      as unrealized_profit
+      , pos_norm.unrealized_profit_pct  as unrealized_profit_pct
 
   from position_normalized pos_norm
 
