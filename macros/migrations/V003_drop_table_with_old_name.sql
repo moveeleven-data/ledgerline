a058c6eb820d5d2512c71027ecd13a716d5262e5
Matthew Tripodi <okv627@gmail.com>
2025-09-24 01:17:44 +0000

    document migration macros and numeric_bounds generic test

--- macros/migrations/V003_drop_table_with_old_name.sql
+++ macros/migrations/V003_drop_table_with_old_name.sql
@@ -1,9 +1,18 @@
+/**
+ * V003_drop_table
+ * ----------------
+ * Migration macro that drops an obsolete table from the refined layer.
+ * - Default target is the current dbt `database` and `schema_prefix`.
+ * - Safe to run multiple times thanks to `IF EXISTS`.
+ * - Used when refactoring / renaming models, so stale physical tables don’t linger.
+ */
+
 {% macro V003_drop_table(
     database = target.database
   , schema_prefix = target.schema
 ) -%}
 
-DROP TABLE IF EXISTS {{database}}.{{schema_prefix
-    }}_REFINED.REF_ABC_BANK_SECURITY_INFO;
+drop table if exists {{database}}.{{schema_prefix
+    }}_REFINED.REF_COUNTRY_ATLAS;
     
 {%- endmacro %}
