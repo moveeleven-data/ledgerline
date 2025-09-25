3121b88be97ae003d788d0f963ba48bce5a6ae07
moveeleven-data <okv627@gmail.com>
2025-09-25 08:27:07 -0400

    Update get_latest_usage_report_date.sql

--- macros/history/get_latest_usage_report_date.sql
+++ macros/history/get_latest_usage_report_date.sql
@@ -12,18 +12,23 @@
 
 {% macro get_latest_usage_report_date() %}
 
-    {# 1. Check for manual override. If the user passed in an override (via --vars), just use that. #}
+    -- 1. Check for manual override. If the user passed in an override (via --vars), just use that.
+    
     {% set as_of_date_override = var('as_of_date', none) %}
     {% if as_of_date_override is not none %}
         {{ return(as_of_date_override) }}
     {% endif %}
 
+
     -- 2. If not running SQL (e.g. docs build), return fallback.
+
     {% if not execute %}
         {{ return(fallback_date_default) }}
     {% endif %}
 
+
     -- 3. Query the latest report_date from source.
+
     {% set latest_report_date_sql %}
         select
               to_char(max(report_date), 'YYYY-MM-DD') as max_report_date
@@ -32,7 +37,9 @@
 
     {% set query_result = run_query(latest_report_date_sql) %}
 
+
     -- 4. Extract result.
+
     {% set latest_report_date_str = (
           query_result
           and query_result.columns
@@ -40,7 +47,9 @@
           and query_result.columns[0].values()[0]
     ) or none %}
 
+
     -- 5. Return result.
+
     {{ return(latest_report_date_str if latest_report_date_str is not none else fallback_date_default) }}
 
 {% endmacro %}
