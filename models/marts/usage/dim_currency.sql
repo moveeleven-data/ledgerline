60e6dc1ea9f0daca3b0869d42d8c928955e8eb97
moveeleven-data <okv627@gmail.com>
2025-09-27 09:04:26 -0400

    Update dim_currency.sql

--- models/marts/usage/dim_currency.sql
+++ models/marts/usage/dim_currency.sql
@@ -20,4 +20,5 @@ select
   , cast(decimal_digits as number(38,0)) as decimal_digits
   , currency_name
 from {{ ref('ref_currency_atlas') }}
-where currency_code is not null
+where 
+    currency_code is not null
