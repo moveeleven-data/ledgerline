fecaa5db8a762e0bffccdd90d5ab65bb1b452815
moveeleven-data <okv627@gmail.com>
2025-09-27 09:04:43 -0400

    Update dim_country.sql

--- models/marts/usage/dim_country.sql
+++ models/marts/usage/dim_country.sql
@@ -44,11 +44,11 @@ select
         ref.country_hkey
       , {{ dbt_utils.generate_surrogate_key(["upper(base.country_code2)"]) }}
     ) as country_key
-
   , base.country_code2 as country_code
   , coalesce(ref.country_name, base.country_name) as country_name
   
 from dim_country_base as base
 left join {{ ref('ref_country_atlas') }} as ref
   on ref.country_code2 = base.country_code2
-where base.country_code2 is not null
+where 
+    base.country_code2 is not null
