9e48493de8745fd08debb6496e814171ee337566
Matthew Tripodi <okv627@gmail.com>
2025-10-17 02:13:03 +0000

    replace countrycode2 with countrycode in dim models

--- models/marts/usage/dim_country.sql
+++ models/marts/usage/dim_country.sql
@@ -8,5 +8,5 @@
 select
       country_hkey as country_key
     , country_name
-    , country_code2 as country_code
+    , country_code
 from {{ ref('ref_country_atlas') }}
--- models/marts/usage/dim_customer.sql
+++ models/marts/usage/dim_customer.sql
@@ -9,5 +9,5 @@ select
       customer_hkey as customer_key
     , customer_code
     , customer_name
-    , country_code2 as country_code
+    , country_code
 from {{ ref('ref_customer_atlas') }}
